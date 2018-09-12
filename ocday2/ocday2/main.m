b6f9d0cb9b102fa725832dfd69dc9062cbc8d4e8
yh-imac <yh-imac@YH-MacBook.local>
2018-09-12 17:53:20 +0800

    ocdaytwo 初始化项目

---
+++ ocday2/ocday2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ocday2
+//
+//  Created by yh-imac on 2018/9/12.
+//  Copyright © 2018年 hselenat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
