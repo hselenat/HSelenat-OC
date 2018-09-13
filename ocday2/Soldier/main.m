dc4e619adbf8e05a97d934206cc201063a0e0ab9
yh-imac <yh-imac@YH-MacBook.local>
2018-09-13 18:42:57 +0800

    daytwo01

---
+++ ocday2/Soldier/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Soldier
+//
+//  Created by yh-imac on 2018/9/13.
+//  Copyright © 2018年 hselenat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
